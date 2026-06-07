4ce53a9490bfbffb1d170e926a58eb2a90abc7c0
shoot1ngstarz <kiritanta2022@gmail.com>
2026-06-07 15:53:34 +0100

    UPDATE
    just furniture and stuff

---
+++ Assets/Scripts/TopDownSorting.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class TopDownSorting : MonoBehaviour
+{
+    private const int Precision = 100;
+    private const int MinOrder = short.MinValue;
+    private const int MaxOrder = short.MaxValue;
+
+    private SpriteRenderer sr;
+    private int lastOrder;
+
+    void Awake()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    void OnEnable()
+    {
+        UpdateSortingOrder();
+    }
+
+    void LateUpdate()
+    {
+        UpdateSortingOrder();
+    }
+
+    private void UpdateSortingOrder()
+    {
+        int order = Mathf.Clamp(-Mathf.RoundToInt(transform.position.y * Precision), MinOrder, MaxOrder);
+        if (order != lastOrder)
+        {
+            sr.sortingOrder = order;
+            lastOrder = order;
+        }
+    }
+}
